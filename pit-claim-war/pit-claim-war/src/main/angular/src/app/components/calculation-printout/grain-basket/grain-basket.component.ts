f1121864bec48e851f43591bdf5488d3b3479810
Vividsolutions/esnauffer <esnauffer@vividsolutions.com>
2025-08-05 12:40:05 -0700

    PIM-2126: Grain Claim Calculator - Grain Basket - Claims UI - Printout

--- pit-claim-war/pit-claim-war/src/main/angular/src/app/components/calculation-printout/grain-basket/grain-basket.component.ts
+++ pit-claim-war/pit-claim-war/src/main/angular/src/app/components/calculation-printout/grain-basket/grain-basket.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { vmCalculation } from 'src/app/conversion/models';
+import { makeTitleCase } from 'src/app/utils';
 
 @Component({
   selector: 'calculation-printout-grain-basket',
@@ -8,4 +9,17 @@ import { vmCalculation } from 'src/app/conversion/models';
 })
 export class CalculationPrintoutGrainBasketComponent {
   @Input() calculationDetail: vmCalculation;
+
+
+  getCmdtyName(str) {
+
+    if (str.indexOf(" - PEDIGREED") > 1 ) {
+      str = str.substring(0, str.indexOf(" - PEDIGREED")) 
+    }
+
+    return makeTitleCase(str)
+  }
+
+  //TODO add a check if there is a pedigreed commodity. If no, then hid the Seeds column
+
 }
