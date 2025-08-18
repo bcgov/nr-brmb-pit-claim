97fc53df9fcdba4c55bd09de7f754ace68e6ed9e
Michael Hostettler <michael.hostettler@gov.bc.ca>
2025-08-18 16:16:23 -0700

    PIM-2111: Enabled create readonly role for TEST and PROD deployment

--- pit-claim-liquibase/scripts/02_00_xx/02_03_01/00/ddl/roles/ccs.ddl.create_role_app_ccs_readonly.sql
+++ pit-claim-liquibase/scripts/02_00_xx/02_03_01/00/ddl/roles/ccs.ddl.create_role_app_ccs_readonly.sql
@@ -1,9 +1,9 @@
 -- Role: "app_ccs_readonly"
 -- DROP ROLE "app_ccs_readonly";
 
---CREATE ROLE "app_ccs_readonly";
+CREATE ROLE "app_ccs_readonly";
 
---GRANT USAGE ON SCHEMA "ccs" TO "app_ccs_readonly";
+GRANT USAGE ON SCHEMA "ccs" TO "app_ccs_readonly";
 
 --Grant read only access to all tables for the readonly role
---GRANT SELECT ON ALL TABLES IN SCHEMA ccs TO "app_ccs_readonly";
+GRANT SELECT ON ALL TABLES IN SCHEMA ccs TO "app_ccs_readonly";
