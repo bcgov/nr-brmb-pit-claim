c928b02fdc1dcab5b69999d5220a50ee903c79ae
mhostett <hostettler.michael@gmail.com>
2025-08-21 14:33:17 -0700

    PIM-2080: Java and spring upgrades

--- cirras-claims-api/cirras-claims-api-rest-endpoints/src/test/java/ca/bc/gov/test/jetty/TestLoginService.java
+++ cirras-claims-api/cirras-claims-api-rest-endpoints/src/test/java/ca/bc/gov/test/jetty/TestLoginService.java
@@ -2,7 +2,7 @@ package ca.bc.gov.test.jetty;
 
 import java.security.Principal;
 
-import jakarta.security.auth.Subject;
+import javax.security.auth.Subject;
 import jakarta.servlet.ServletRequest;
 
 import org.eclipse.jetty.security.DefaultIdentityService;
