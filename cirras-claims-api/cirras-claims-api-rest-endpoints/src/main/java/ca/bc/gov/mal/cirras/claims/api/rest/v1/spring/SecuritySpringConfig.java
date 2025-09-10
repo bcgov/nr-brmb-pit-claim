942c6bf53e5e0d33f4dc2d0b7192ac920fd27f2a
David Pitkin <dpitkin@vividsolutions.com>
2025-09-10 15:34:18 -0700

    PIM-2080: Updated SecuritySpringConfig to more closely match previous logic.

--- cirras-claims-api/cirras-claims-api-rest-endpoints/src/main/java/ca/bc/gov/mal/cirras/claims/api/rest/v1/spring/SecuritySpringConfig.java
+++ cirras-claims-api/cirras-claims-api-rest-endpoints/src/main/java/ca/bc/gov/mal/cirras/claims/api/rest/v1/spring/SecuritySpringConfig.java
@@ -106,6 +106,7 @@ public class SecuritySpringConfig  {
 	  @Bean
 	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable())
+		  .cors(cors -> cors.disable())
 	      .oauth2ResourceServer(oauth2 -> oauth2.authenticationManagerResolver(authenticationManagerResolver()) )
 	      .httpBasic(Customizer.withDefaults())
 	      .authorizeHttpRequests(authorize -> authorize
