33ee240687b4c178fff98c16df980770f2050e02
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-23 16:01:50 -0700

    PIM-2580: Implement AppSecurityProperties.

---
+++ cirras-claims-api/cirras-claims-api-server/src/main/java/ca/bc/gov/mal/cirras/claims/spring/AppSecurityProperties.java
@@ -0,0 +1,26 @@
+package ca.bc.gov.mal.cirras.claims.spring;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AppSecurityProperties {
+
+    private final Map<String, List<String>> roleScopeMappings;
+
+    public AppSecurityProperties(
+            @Value("${app.security.role-scope-mappings}") String jsonMappings,
+            ObjectMapper objectMapper) throws Exception {
+        this.roleScopeMappings = objectMapper.readValue(jsonMappings, new TypeReference<>() {
+        });
+    }
+
+    public List<String> getScopesForRole(String role) {
+        String cleanRole = role.replace("ROLE_", "");
+        return roleScopeMappings.getOrDefault(cleanRole, List.of());
+    }
+}
