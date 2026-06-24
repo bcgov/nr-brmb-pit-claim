12d0492597350f8fe71895c3a737ed70e1504ba2
Hong-Yi Wang <hwang@vividsolutions.com>
2026-06-24 09:38:06 -0700

    PIM-2580: Remove unused import.

--- cirras-claims-api/cirras-claims-api-server/src/main/java/ca/bc/gov/mal/cirras/claims/spring/SecuritySpringConfig.java
+++ cirras-claims-api/cirras-claims-api-server/src/main/java/ca/bc/gov/mal/cirras/claims/spring/SecuritySpringConfig.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.AuthenticationManagerResolver
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.Authentication;
