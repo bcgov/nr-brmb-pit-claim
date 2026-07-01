83be0de8f93ac75e4f6b18b1f16c99b45b345733
Vividsolutions/esnauffer <esnauffer@vividsolutions.com>
2026-06-30 17:04:12 -0700

    Express is replacing the env variables but it needs more tweaking

--- pit-claim-ui/express-server/server.js
+++ pit-claim-ui/express-server/server.js
@@ -3,12 +3,42 @@ const http = require('http');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 
 // Host setup
 const port = process.env.PORT || 8080;
 //const hostname = "0.0.0.0";
+
+// Env values to replace in appConfig.json
+// This code updates the file once, when the Node server starts
+// needs a bit of tweaking - right now it has trouble replacing WEBADE_OAUTH2_SCOPES
+const configPath = path.join(__dirname, '../angular/dist/pit-claim/assets/data/appConfig.json');
+
+try {
+
+	let config = fs.readFileSync(configPath, 'utf8');
+
+	config = config
+		.replace(/#\{BASE_URL\}#/g, process.env.BASE_URL)
+		.replace(/#\{ENV\}#/g, process.env.ENV )
+		.replace(/#\{CIRRAS_CLAIMS_REST_URI\}#/g, process.env.CIRRAS_CLAIMS_REST_URI)
+		.replace(/#\{PIT_UNDERWRITING_UI_URL\}#/g, process.env.PIT_UNDERWRITING_UI_URL)
+		.replace(/#\{WEBADE_OAUTH2_AUTHORIZE_URL\}#/g, process.env.WEBADE_OAUTH2_AUTHORIZE_URL)
+		.replace(/#\{CIRRAS_CLAIMS_UI\}#/g, process.env.CIRRAS_CLAIMS_UI)
+		.replace(/#\{WEBADE_OAUTH2_SCOPES\}#/g, process.env.WEBADE_OAUTH2_SCOPES)
+		.replace(/#\{WEBADE_OAUTH2_ENABLE_CHECKTOKEN\}#/g, process.env.WEBADE_OAUTH2_ENABLE_CHECKTOKEN )
+		.replace(/#\{UI_CHECKTOKEN_ENDPOINT\}#/g, process.env.UI_CHECKTOKEN_ENDPOINT )
+		.replace(/#\{WEBADE_OAUTH2_SITEMINDER_URL\}#/g, process.env.WEBADE_OAUTH2_SITEMINDER_URL );
+
+	fs.writeFileSync(configPath, config);
+
+    console.log(`Updated appConfig.json`);
+    //console.log(`ENV: ${config.environment}`);
+} catch (err) {
+    console.error(`Failed to update ${configPath}:`, err.message);
+}
 
 // Middleware
 app.use(cors());
