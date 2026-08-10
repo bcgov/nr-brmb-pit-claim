090aa974a8063867c2935d08c665a0837ad9a2b6
vivid-esnauffer <esnauffer@vividsolutions.com>
2026-08-10 14:52:04 -0700

    Updated docker and server.js

--- pit-claim-ui/express-server/server.js
+++ pit-claim-ui/express-server/server.js
@@ -10,38 +10,10 @@ const app = express();
 const port = process.env.TOMCAT_PORT || 8080;
 const hostname = "0.0.0.0";
 
-const angularPath = path.join(__dirname, '../angular/dist/pit-claim');
-
-// function setEnvironmentValues() {
-// 	// Env values to replace in appConfig.json
-// 	// This code updates the file only once, when the Node server starts
-	
-// 	console.log(`Updating appConfig.json`);
-	
-// 	const configPath = path.join(angularPath, '/assets/data/appConfig.json');
-
-// 	let config = fs.readFileSync(configPath, 'utf8');
-
-// 	config = config
-// 		.replace(/#\{BASE_URL\}#/g, process.env.BASE_URL)
-// 		.replace(/#\{APPLICATION_ENVIRONMENT_NAME\}#/g, process.env.APPLICATION_ENVIRONMENT_NAME )
-// 		.replace(/#\{CIRRAS_CLAIMS_REST_URI\}#/g, process.env.CIRRAS_CLAIMS_REST_URI)
-// 		.replace(/#\{PIT_UNDERWRITING_UI_URL\}#/g, process.env.PIT_UNDERWRITING_UI_URL)
-// 		.replace(/#\{WEBADE_OAUTH2_AUTHORIZE_URL\}#/g, process.env.WEBADE_OAUTH2_AUTHORIZE_URL)
-// 		.replace(/#\{CIRRAS_CLAIMS_UI\}#/g, process.env.CIRRAS_CLAIMS_UI)
-// 		.replace(/#\{WEBADE_OAUTH2_SCOPES\}#/g, process.env.WEBADE_OAUTH2_SCOPES)
-// 		.replace(/#\{WEBADE_OAUTH2_ENABLE_CHECKTOKEN\}#/g, process.env.WEBADE_OAUTH2_ENABLE_CHECKTOKEN )
-// 		.replace(/#\{UI_CHECKTOKEN_ENDPOINT\}#/g, process.env.UI_CHECKTOKEN_ENDPOINT )
-// 		.replace(/#\{WEBADE_OAUTH2_SITEMINDER_URL\}#/g, process.env.WEBADE_OAUTH2_SITEMINDER_URL );
-
-// 	fs.writeFileSync(configPath, config);
-
-// 	console.log(`Updated appConfig.json`);
-// };
+const angularPath = path.join(__dirname, '../angular/dist/pit-claim-ui');
 
 function startServer() {
 	try {
-		// setEnvironmentValues();
 
 		console.log(`Updating appConfig.json`);
 
